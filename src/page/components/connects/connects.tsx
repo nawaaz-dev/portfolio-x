eaafe7ec8805dc7015bec1c28a1f304c90d34e7e
nawaazkortiwala <nawaazkortiwala@gmail.com>
2025-02-14 15:04:22 +0530

    added connects

--- src/page/components/connects/connects.tsx
+++ src/page/components/connects/connects.tsx
@@ -1,7 +1,48 @@
 import { FC } from "react";
 
 const Connects: FC = () => {
-  return <div>Connects</div>;
+  const connects = [
+    {
+      title: "X",
+      url: "https://x.com",
+    },
+    {
+      title: "Medium",
+      url: "https://medium.com",
+    },
+    {
+      title: "UpWork",
+      url: "https://upwork.com",
+    },
+    {
+      title: "Fiverr",
+      url: "https://fiverr.com",
+    },
+    {
+      title: "GitHub",
+      url: "https://github.comm",
+    },
+    {
+      title: "LinkedIn",
+      url: "https://linkedin.com",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-3 gap-4 p-4">
+      {connects.map((connect) => (
+        <a
+          key={connect.title}
+          href={connect.url}
+          target="_blank"
+          rel="noreferrer"
+          className="underline text-sm"
+        >
+          {connect.title}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Connects;
